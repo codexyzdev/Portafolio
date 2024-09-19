3ce87ef4ff10b96c8c437cde0c3c91d599919f24
alejandrobaez <alejandrobaez938@gmail.com>
2024-09-19 15:56:31 -0400

    add vercel log

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import icon from "astro-icon";
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import vercel from "@astrojs/vercel/serverless";
 
@@ -9,5 +9,7 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [tailwind(), icon(), compress()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
